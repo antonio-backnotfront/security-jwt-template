9c412a30da3f673be1acb80c70f357024150d252
Anton Solianyk <solyanicks@gmail.com>
2025-07-24 10:10:38 +0200

    init

---
+++ src/test/java/com/example/securityjwttemplate/SecurityJwtTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securityjwttemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityJwtTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
